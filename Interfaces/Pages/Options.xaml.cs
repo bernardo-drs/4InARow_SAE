d5abbb520e5b1463c9a072e51ce53479ce394ee9
Clémence Dolidon <clemdolidon@gmail.com>
2026-05-26 21:31:41 +0200

    Changement couleur, taille de police, bouton appliquer les changements fonctionnels

--- Interfaces/Pages/Options.xaml.cs
+++ Interfaces/Pages/Options.xaml.cs
@@ -10,17 +10,58 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Systeme;
 
 namespace Interfaces.Pages
 {
-    /// <summary>
-    /// Interaction logic for Options.xaml
-    /// </summary>
     public partial class Options : Page
     {
+        private readonly OptionManager _manager = new();
+
         public Options()
         {
             InitializeComponent();
+        }
+
+        private void BtnAppliquerOptions_Click(object sender, RoutedEventArgs e)
+        {
+            _manager.AppliquerTailleTexte(SliderTailleTexte.Value);
+
+            var brushContraste = _manager.AppliquerContraste(SliderContraste.Value);
+            this.Background = brushContraste;
+
+            string? nomCouleur = GetCouleurCochee();
+            if (nomCouleur != null)
+            {
+                var brushCouleur = _manager.AppliquerCouleurFond(nomCouleur);
+                if (brushCouleur != null)
+                    this.Background = brushCouleur;
+            }
+
+            _manager.AppliquerFormeJeton(GetFormeCochee());
+        }
+
+        private void BtnFermer_Click(object sender, RoutedEventArgs e)
+        {
+            if (NavigationService?.CanGoBack == true)
+                NavigationService.GoBack();
+        }
+
+        private string? GetCouleurCochee()
+        {
+            var boutons = new[] { CouleurBleuFonce, CouleurMauve, CouleurVert,
+                                  CouleurOrange, CouleurJaune, CouleurBleuClair };
+            foreach (var rb in boutons)
+                if (rb.IsChecked == true) return rb.Name;
+            return null;
+        }
+
+        private string GetFormeCochee()
+        {
+            if (FormeCarré.IsChecked == true) return "Carré";
+            if (FormeEtoile.IsChecked == true) return "Etoile";
+            if (FormeTriangle.IsChecked == true) return "Triangle";
+            return "Rond";
         }
     }
 }
