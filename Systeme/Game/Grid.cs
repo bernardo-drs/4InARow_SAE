5aa7806b2479e7499ffca49cd535ffc0a99db439
Bernardo <b.m.d.simoes@gmail.com>
2026-05-28 13:11:28 +0200

    Merge IA or Human

---
+++ Systeme/Game/Grid.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Systeme.Game
+{
+    internal class Grid
+    {
+    }
+}
---
+++ Systeme/Game/Jeton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Systeme.Game
+{
+    internal class Jeton
+    {
+        public Jeton()
+        {
+
+        }
+    }
+}
